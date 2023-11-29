8186cddc94801ccc531e580ca011b14335c8f0fb
Sean Johnson <seanjohnson@Seans-MBP.attlocal.net>
2023-11-29 18:30:38 -0500

    Adds sailboat ASCII art.

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ const messageData = {
         'freedom, open air, and adventure are found on the sea.',
         'seas the day!',
         'home is where the anchor drops.'],
+    sailboatArt: [
+        'v  ~.      v',
+        'v           /|',
+        '           / |          v',
+        '    v     /__|__',
+        '        \\--------/',
+        '~~~~~~~~~~~~~~~~~~~~~~~~~'],
     getTemperature() { //Returns temperature between 0 & 100 degrees Farenheight as a number.
         return Math.floor(Math.random()*101);
     },
@@ -27,16 +34,27 @@ const messageData = {
     }
 };
 
+//Function that prints the sailboatArt array.
+const printAsciiArt = (arr) => {
+    for (i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
 //Function that logs the randomized message to the console.
 const logRandomMessage = () => {
     const randomSailboat = messageData.sailboat[Math.floor(Math.random() * messageData.sailboat.length)];
     const randomQuote = messageData.sailingQuotes[Math.floor(Math.random() * messageData.sailingQuotes.length)];
     const randomTemp = messageData.getTemperature();
     const randomWindSpeed = messageData.getWindSpeed();
+    console.log('');
     console.log(`Ahoy, welcome to the sailing life!`);
     console.log(`You'll be sailing ${randomSailboat}.`);
     console.log(`The temperature is forecast to be ${randomTemp} degrees Fahrenheit with winds maxing out at ${randomWindSpeed}kts!`);
     console.log(`This should be a peace of cake, and remember "${randomQuote}"`);
+    console.log('');
+    printAsciiArt(messageData.sailboatArt);
+    console.log('');
 }
 
 logRandomMessage();
