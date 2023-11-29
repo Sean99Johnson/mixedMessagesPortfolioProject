e580da17e52d269c762e6849d8cc4516e95d24d4
Sean Johnson <seanjohnson@Seans-MacBook-Pro.local>
2023-11-29 15:37:29 -0500

    Creates object that stores data and methods for creating the random message.

---
+++ script.js
@@ -0,0 +1,11 @@
+//Object that stores data and methods for creating the random message.
+const messageData = {
+    sailboat: [],
+    sailingQuotes: [],
+    getTemperature() {
+
+    },
+    getWindSpeed() {
+        
+    }
+};
